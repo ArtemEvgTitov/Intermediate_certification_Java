e0745f347d7274e0d23bbb6b108b52323075afdc
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-03-19 13:49:28 +0300

    Добавлены конструкторы и методы для игрушки

--- Toy_draw/Model/Toy.java
+++ Toy_draw/Model/Toy.java
@@ -13,4 +13,43 @@ public class Toy extends ToyModel {
         this.quantity = quantity;
         this.frequency = frequency;
     }
+
+    public Toy(String name, int quantity, int frequency){
+        this.name = name;
+        this.quantity = quantity;
+        this.frequency = frequency;
+    }
+
+    public int getID(){
+        return id;
+    }
+
+    public void setID(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    public int getFrequency(){
+        return frequency;
+    }
+
+    public void setFrequency(int frequency){
+        this.frequency = frequency;
+    }
+
 }
